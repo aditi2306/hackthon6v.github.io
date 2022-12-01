d066c94494da6b3c615b9bc2b2c9d06de9acf9c2
aditi2306 <35637439+aditi2306@users.noreply.github.com>
2022-12-01 21:59:50 +0530

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,23 +1,17 @@
-const staticDevCoffee = "dev-coffee-site-v1";
+const staticDev = "dev-employee-pwc-site-v1";
 const assets = [
   "/",
   "/index.html",
   "/css/style.css",
   "/js/app.js",
-  "/images/coffee1.jpg",
-  "/images/coffee2.jpg",
-  "/images/coffee3.jpg",
-  "/images/coffee4.jpg",
-  "/images/coffee5.jpg",
-  "/images/coffee6.jpg",
-  "/images/coffee7.jpg",
-  "/images/coffee8.jpg",
-  "/images/coffee9.jpg"
+  "/images/logo.jpg"
 ];
+
+const test =document.getElementById("install-id");
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticDev).then(cache => {
       cache.addAll(assets);
     })
   );
